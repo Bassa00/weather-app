dd2443d3cf47c978397c63b2796afc8d10259afc
brianttocs <brianttocs@gmail.com>
2019-02-16 21:16:40 +0200

    main files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Title from './components/Title'
+import Form from './components/Form'
+import Weather from './components/Weather'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Title />
+        <Form />
+        <Weather/>
       </div>
     );
   }
