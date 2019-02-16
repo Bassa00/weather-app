d0df8c2b43aea0973851e78d7437c0b69c5a6d27
brianttocs <brianttocs@gmail.com>
2019-02-16 22:58:11 +0200

    form data picking

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ const API_KEY = 'daa0eb66357a8dad748239018d032f0d'
 class App extends Component {
   getWeather = async (e) => {
     e.preventDefault();
-    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}&units=metric`);
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
 
     const data = await api_call.json();
 
